102170ff6de28ba9ce8540216686c325b5897802
Eric Y <ericy1042@gmail.com>
2025-05-14 22:45:31 -0500

    finished coding for account-service, TODO: Postman testing

---
+++ account-service/src/main/java/com/small/backend/accountservice/entity/PaymentMethod.java
@@ -0,0 +1,8 @@
+package com.small.backend.accountservice.entity;
+
+public enum PaymentMethod {
+    CREDIT_CARD,
+    DEBIT_CARD,
+    PAYPAL,
+    BANK_TRANSFER
+}
---
+++ account-service/src/main/java/com/small/backend/accountservice/service/AccountService.java
@@ -0,0 +1,14 @@
+package com.small.backend.accountservice.service;
+
+import com.small.backend.accountservice.dto.CreateAccountRequest;
+import com.small.backend.accountservice.dto.UpdateAccountRequest;
+import com.small.backend.accountservice.entity.UserAccount;
+
+import java.util.UUID;
+
+public interface AccountService {
+    UserAccount createAccount(CreateAccountRequest request);
+    UserAccount updateAccount(UUID id, UpdateAccountRequest request);
+    UserAccount getAccountById(UUID id);
+    UserAccount getAccountByEmail(String email);
+}
