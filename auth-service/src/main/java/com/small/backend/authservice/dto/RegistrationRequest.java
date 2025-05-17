a7d232b0439edbc42ab7ee45c55e7b5a8a589708
Eric Y <ericy1042@gmail.com>
2025-05-17 00:25:09 -0500

    security-related code for account-service (but only validates access token), internal auth token added TODO: test in Postman, refresh token validation in account-service

---
+++ auth-service/src/main/java/com/small/backend/authservice/dto/RegistrationRequest.java
@@ -0,0 +1,78 @@
+package com.small.backend.authservice.dto;
+
+import entity.PaymentMethod;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class RegistrationRequest {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 12)
+    private String password;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String shippingAddress;
+
+    @NotBlank
+    private String billingAddress;
+
+    @NotNull
+    private PaymentMethod paymentMethod;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public String getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+}
