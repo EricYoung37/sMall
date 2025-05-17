9365372f07b573d85b22d7f2a4aa03ef92cad802
Eric Y <ericy1042@gmail.com>
2025-05-17 01:05:44 -0500

    user can only access their own account TODO: test in Postman, refresh token validation in account-service

--- account-service/src/main/java/com/small/backend/accountservice/controller/AccountController.java
+++ account-service/src/main/java/com/small/backend/accountservice/controller/AccountController.java
@@ -1,13 +1,14 @@
 package com.small.backend.accountservice.controller;
 
 import dto.CreateAccountRequest;
-import dto.RegistrationRequest;
 import com.small.backend.accountservice.dto.UpdateAccountRequest;
 import com.small.backend.accountservice.entity.UserAccount;
 import com.small.backend.accountservice.service.AccountService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -23,23 +24,44 @@ public class AccountController {
         this.service = service;
     }
 
-    @PostMapping
+    @PostMapping // internal authentication enforced by auth-service
     public ResponseEntity<UserAccount> create(@RequestBody @Valid CreateAccountRequest request) {
         return ResponseEntity.ok(service.createAccount(request));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<UserAccount> update(@PathVariable("id") UUID id, @RequestBody @Valid UpdateAccountRequest request) {
+    public ResponseEntity<Object> update(@PathVariable("id") UUID id,
+                                              @RequestBody @Valid UpdateAccountRequest request,
+                                              Authentication auth) {
+        UserAccount userAccount = service.getAccountById(id);
+        if (!auth.getName().equals(userAccount.getEmail())) {
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body("Access Denied.");
+        }
         return ResponseEntity.ok(service.updateAccount(id, request));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserAccount> getById(@PathVariable("id") UUID id) {
-        return ResponseEntity.ok(service.getAccountById(id));
+    public ResponseEntity<Object> getById(@PathVariable("id") UUID id,
+                                               Authentication auth) {
+        UserAccount userAccount = service.getAccountById(id);
+        if (!auth.getName().equals(userAccount.getEmail())) {
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body("Access Denied.");
+        }
+        return ResponseEntity.ok(userAccount);
     }
 
     @GetMapping("/email/{email}")
-    public ResponseEntity<UserAccount> getAccountByEmail(@PathVariable("email") String email) {
+    public ResponseEntity<Object> getAccountByEmail(@PathVariable("email") String email,
+                                                         Authentication auth) {
+        if (!auth.getName().equals(email)) {
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body("Access Denied.");
+        }
         UserAccount userAccount = service.getAccountByEmail(email);
         return ResponseEntity.ok(userAccount);
     }
