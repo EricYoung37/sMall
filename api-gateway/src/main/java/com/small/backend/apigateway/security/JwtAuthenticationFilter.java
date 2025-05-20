75c71789e7c678cf8705e54f5a80c822a1bcfbe8
Eric Y <ericy1042@gmail.com>
2025-05-20 16:20:27 -0500

    response with a message if auth token is absent

--- api-gateway/src/main/java/com/small/backend/apigateway/security/JwtAuthenticationFilter.java
+++ api-gateway/src/main/java/com/small/backend/apigateway/security/JwtAuthenticationFilter.java
@@ -7,12 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 import util.AppConstants;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
@@ -37,9 +42,23 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
 
         // Check for Authorization header
         final String authHeader = exchange.getRequest().getHeaders().getFirst(AppConstants.AUTHORIZATION_HEADER);
-        if (authHeader == null || !authHeader.startsWith(AppConstants.BEARER_PREFIX)) {
+        if (authHeader == null || !authHeader.startsWith(AppConstants.BEARER_PREFIX)) { // absent token
+
+            // Let the frontend handle redirect.
+            // If "Automatically follow redirects" is turned on in Postman,
+            // this will fail because the login endpoint expects a JSON request body.
+            // exchange.getResponse().setStatusCode(HttpStatus.FOUND); // 302 redirect
+            // exchange.getResponse().getHeaders().set(HttpHeaders.LOCATION, "/api/v1/auth/login");
+            // return exchange.getResponse().setComplete();
+
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            return exchange.getResponse().setComplete();
+            exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+
+            String message = "Missing or invalid Authorization header. Expected format: 'Authorization: Bearer <token>'";
+            DataBufferFactory bufferFactory = exchange.getResponse().bufferFactory();
+            DataBuffer buffer = bufferFactory.wrap(message.getBytes(StandardCharsets.UTF_8));
+
+            return exchange.getResponse().writeWith(Mono.just(buffer));
         }
 
         // Validate JWT
@@ -60,6 +79,7 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
             return chain.filter(mutatedExchange);
         } catch (SignatureException | IllegalArgumentException ex) {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            // May return a message as in the case of absent token.
             return exchange.getResponse().setComplete();
         }
     }
